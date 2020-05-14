a408c1f7ae3ceb51a54d6f10a39aa302dca5b427
Toshiki Teramura <toshiki.teramura@gmail.com>
2020-05-14 11:31:35 +0900

    Move examples to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,63 @@
+//! Python-like decorator for Rust
+//!
+//! Example
+//! --------
+//!
+//! ```
+//! use deco::deco;
+//!
+//! fn logging<F>(func: F) -> impl Fn(i32) -> i32
+//! where
+//!     F: Fn(i32) -> i32,
+//! {
+//!     move |i| {
+//!         println!("Input = {}", i);
+//!         let out = func(i);
+//!         println!("Output = {}", out);
+//!         out
+//!     }
+//! }
+//!
+//! #[deco(logging)]
+//! fn add2(i: i32) -> i32 {
+//!     i + 2
+//! }
+//!
+//! add2(2);
+//! ```
+//!
+//! - Decorator with parameter
+//!
+//! ```
+//! use deco::deco;
+//! use std::{fs, io::Write};
+//!
+//! fn logging<InputFunc: 'static>(
+//!     log_filename: &'static str,
+//! ) -> impl Fn(InputFunc) -> Box<dyn Fn(i32) -> i32>
+//! where
+//!     InputFunc: Fn(i32) -> i32,
+//! {
+//!     move |func: InputFunc| {
+//!         Box::new(move |i: i32| {
+//!             let mut f = fs::File::create(log_filename).unwrap();
+//!             writeln!(f, "Input = {}", i).unwrap();
+//!             let out = func(i);
+//!             writeln!(f, "Output = {}", out).unwrap();
+//!             out
+//!         })
+//!     }
+//! }
+//!
+//! #[deco(logging("test.log"))]
+//! fn add2(i: i32) -> i32 {
+//!     i + 2
+//! }
+//!
+//! add2(2);
+//! ```
+//!
+
 use anyhow::{bail, Result};
 use proc_macro::TokenStream;
 use proc_macro2::TokenTree;
